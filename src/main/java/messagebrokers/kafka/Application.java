b3fd82bafa4f156a2b09627142de7dbb22248c80
ChanManChan <ngopal253@gmail.com>
2021-12-14 00:13:35 +0530

    working-kafka-producer

--- src/main/java/messagebrokers/kafka/Application.java
+++ src/main/java/messagebrokers/kafka/Application.java
@@ -9,7 +9,12 @@ import java.util.concurrent.ExecutionException;
 
 public class Application {
     private static final String TOPIC = "events";
-    private static final String BOOTSTRAP_SERVERS = "localhost:9092, localhost:9093, localhost:9094";
+    // If you're running your Apache Kafka brokers using WSL2 and want to connect Java service to your brokers, you may run into some errors due to misconfigurations.
+    // For instance, trying to connect to "localhost:9092" or "127.0.0.1:9092" will not work.
+    // Solution - IPv6
+    // Use IPv6 loopback address in your broker (server.properties)
+    // listeners=PLAINTEXT://[::1]:9092
+    private static final String BOOTSTRAP_SERVERS = "[::1]:9092,[::1]:9093,[::1]:9094";
 
     public static void main(String[] args) {
         Producer<Long, String> kafkaProducer = createKafkaProducer(BOOTSTRAP_SERVERS);
@@ -26,12 +31,21 @@ public class Application {
     }
 
     public static void produceMessages(int numberOfMessages, Producer<Long, String> kafkaProducer) throws ExecutionException, InterruptedException {
-        int partition = 0;
+        // int partition = 0; <- if we do not specify the partition explicitly, a hash function is going to be applied on the key which will determine which partition the event will go to
         for (int i = 0; i < numberOfMessages; i++) {
-            long key = i;
+            // long key = i;
             String value = String.format("event %d", i);
-            long timeStamp = System.currentTimeMillis();
-            ProducerRecord<Long, String> producerRecord = new ProducerRecord<>(TOPIC, partition, timeStamp, key, value);
+            // long timeStamp = System.currentTimeMillis();
+
+            // Generally using the key to determine the target partition is a better approach rather than choosing a partition explicitly because when using
+            // the key we don't need to know any specifics about the topic structure and how many partitions it has. So we can run a producer that is less coupled to the topic configuration.
+            // ProducerRecord<Long, String> producerRecord = new ProducerRecord<>(TOPIC, partition, timeStamp, key, value);
+
+            // In some cases having a key and a value just doesn't make any logical sense, in this case we can publish the event with no key and the kafka producer will simply use round-robin
+            // to decide to which partition it is going to send the messages to.
+            // ProducerRecord<Long, String> producerRecord = new ProducerRecord<>(TOPIC, key, value);
+
+            ProducerRecord<Long, String> producerRecord = new ProducerRecord<>(TOPIC, value);
             RecordMetadata recordMetadata = kafkaProducer.send(producerRecord).get();
             System.out.printf("Record with (key: %s, value: %s), was sent to (partition: %d, offset: %d)%n", producerRecord.key(), producerRecord.value(), recordMetadata.partition(), recordMetadata.offset());
         }
